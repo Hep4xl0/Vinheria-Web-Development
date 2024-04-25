409d47bd57ae4690e995f64179a7f838f1929fb6
sannunez <sftwr.gsantosnunes210@gmail.com>
2024-04-25 18:31:03 -0300

    Atualização de Pagina de Login

--- Vinheria Agnello/Vinheria Completa.js
+++ Vinheria Agnello/Vinheria Completa.js
@@ -70,3 +70,27 @@ function caixaCupom(){
 function mostravalor() {
     valor.innerHTML= `R$ ${total},00`;
 }
+
+
+// LOGIN 
+
+
+// WHILE 
+function login(){
+    let nomeUsuario;
+    let senhaUsuario;
+
+    while(true){
+        nomeUsuario = document.getElementById("login").value;
+        senhaUsuario = document.getElementById("senha").value;
+
+        if(nomeUsuario === "1234" && senhaUsuario === "1234"){
+            alert("Logado com sucesso!");
+            break
+        }
+
+        alert("Login ou senha invalidos!")
+        break
+    }
+}
+
